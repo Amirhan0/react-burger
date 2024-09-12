207f41a869dbad4c45c401e758b5b4d5be4e17eb
Amirhan0 <amirgasanov246@gmail.com>
2024-09-12 11:41:58 +0600

    registration

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,59 @@
 const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "react_burger",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Ошибка в подключении к базе данных SQL");
+    return;
+  }
+  console.log("Подключение к базе данных успешна");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  const { email, phoneNumber, passwordUser } = req.body;
+
+  if (!email || !phoneNumber || !passwordUser) {
+    return res.status(400).json({ message: "Все поля обязательны" });
+  }
+
+  const query =
+    "INSERT INTO users (email, phoneNumber, passwordUser) VALUES (?, ?, ?)";
+
+  db.query(query, [email, phoneNumber, passwordUser], (err, result) => {
+    if (err) {
+      console.log("Ошибка регистрации", err);
+      return res.status(500).json({ message: "Ошибка при регистрации" });
+    }
+    res
+      .status(200)
+      .json({ message: "Пользователь успешно зарегистрирован!" });
+  });
+});
+
+app.get("/users", (req, res) => {
+  const query = "SELECT id, email, phoneNumber, passwordUser FROM users";
+
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log("Ошибка при получение пользователя");
+      return;
+    }
+    res.status(200).json(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Сервер запущен");
+});
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,72 +1,133 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Registration() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
 
   function navigation(link) {
-    navigate(link)
+    navigate(link);
   }
+
+  const validate = () => {
+    const errors = {};
+
+    if (!email) {
+      errors.email = "Введите почту ";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = "Неккоректный адрес почты";
+    }
+
+    if (!phone) {
+      errors.phone = "Введите пароль";
+    } else if (!/^\d{11}$/.test(phone)) {
+      errors.phone = "Неккоректный формат номера";
+    }
+
+    if (!password) {
+      errors.password = "Введите пароль";
+    } else if (password.length < 6) {
+      errors.password = "Пароль должен содержать не менее 6 символов";
+    }
+
+    return errors;
+  };
+
+  const submitHandle = async (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:3001/register", {
+        email,
+        phoneNumber: phone,
+        passwordUser: password,
+      });
+      console.log("Пользователь зарегистрирован", response.data);
+      navigation("/fullname");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="relative flex flex-col bg-[url('/bg.JPG')] w-screen h-screen justify-center items-center text-white  px-6 py-14">
+    <div className="relative flex flex-col bg-[url('/bg.JPG')] w-screen h-screen justify-center items-center text-white px-6 py-14">
       <div className="absolute inset-0 bg-gray-900 opacity-80"></div>
       <div className="flex flex-col justify-evenly items-center h-screen">
-        <div className="relative flex flex-col justify-center items-center ">
-        <h1 className="font-ar-cena text-4xl font-bold">BURGER BAR</h1>
-      </div>
-      <div className=" relative flex flex-col">
-        <div className=" relative flex flex-col items-center  justify-center pb-10">
-          <h2 className="font-bold text-2xl">Создать аккаунт</h2>
-          <p className="text-[#6C7072] text-sm">Зарегистрируйтесь, чтобы продолжить</p>
+        <div className="relative flex flex-col justify-center items-center">
+          <h1 className="font-ar-cena text-4xl font-bold">BURGER BAR</h1>
         </div>
-        {/* inputs */}
-        <div className="gap-3 flex flex-col ">
-          {/* инпут 1 */}
-          <div className="bg-[#22222A]  py-4 w-full rounded-xl flex px-3 gap-4 items-center">
-            <img src="/paperPlane.svg" alt="" />
-            <input
-              type="text"
-              placeholder="Введите почту"
-              className="outline-none bg-transparent flex pb-[3px] w-full placeholder-white"
-            />
+        <div className="relative flex flex-col">
+          <div className="relative flex flex-col items-center justify-center pb-10">
+            <h2 className="font-bold text-2xl">Создать аккаунт</h2>
+            <p className="text-[#6C7072] text-sm">
+              Зарегистрируйтесь, чтобы продолжить
+            </p>
           </div>
-          {/* инпут 2 */}
-          <div className="bg-[#22222A]  py-4 w-full rounded-xl flex px-3 gap-4 items-center">
-            <img src="/phone.svg" alt="" />
-            <input
-              type="number"
-              placeholder="Введите номер"
-              className="outline-none bg-transparent flex pb-[3px] w-full placeholder-white"
-            />
-          </div>
-          {/* инпут 3 */}
-          <div className="bg-[#22222A]  py-4 w-full rounded-xl flex px-3 gap-4 items-center">
-            <img src="/Shield.svg" alt="" />
-            <input
-              type="password"
-              placeholder="Введите пароль"
-              className="outline-none bg-transparent flex pb-[3px] w-full placeholder-white"
-            />
-            <img src="/eye-off.svg" alt="" />
-          </div>
+          <form className="gap-3 flex flex-col" onSubmit={submitHandle}>
+            <div className="bg-[#22222A] py-4 w-full rounded-xl flex px-3 gap-4 items-center">
+              <img src="/PaperPlane.svg" alt="" />
+              <input
+                type="text"
+                placeholder="Введите почту"
+                className="outline-none bg-transparent flex pb-[3px] w-full placeholder-white"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+            <div className="bg-[#22222A] py-4 w-full rounded-xl flex px-3 gap-4 items-center">
+              <img src="/Phone.svg" alt="" />
+              <input
+                type="text"
+                placeholder="Введите номер"
+                className="outline-none bg-transparent flex pb-[3px] w-full placeholder-white"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            {errors.phone && (
+              <p className="text-red-500 text-sm">{errors.phone}</p>
+            )}
+            <div className="bg-[#22222A] py-4 w-full rounded-xl flex px-3 gap-4 items-center">
+              <img src="/Shield.svg" alt="" />
+              <input
+                type="password"
+                placeholder="Введите пароль"
+                className="outline-none bg-transparent flex pb-[3px] w-full placeholder-white"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <img src="/eye-off.svg" alt="" />
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password}</p>
+            )}
+            <button
+              className="bg-custom-gradient w-full p-3 text-black font-bold rounded-full outline-none text-xl mt-4"
+              type="submit"
+            >
+              Создать аккаунт
+            </button>
+          </form>
+          <button
+            className="bg-transparent text-white font-bold rounded-full outline-none text-xl mt-4"
+            onClick={() => navigation("/authorization")}
+          >
+            Уже есть аккаунт? <span className="text-[#B28C2E]">Войти</span>
+          </button>
         </div>
-        {/* <div className="flex flex-col mt-5">
-          <ul className="list-disc pl-5 text-sm text-[#1EC625]">
-            <li>Пароль содержит более 3 символов</li>
-            <li>Введены специальные символы</li>
-          </ul>
-        </div> */}
       </div>
-      <div className="flex flex-col gap-4 relative">
-        <button className="bg-custom-gradient w-full p-3 text-black font-bold rounded-full outline-none text-xl" onClick={() => navigation('/fullname')}>
-          Создать аккаунт
-        </button>
-
-        <button className="bg-transparent  text-white font-bold rounded-full outline-none text-xl">
-          Уже есть аккаунт? <span className="text-[#B28C2E]" onClick={() => navigation('/authorization')}>Войти</span>
-        </button>
-      </div>
-      </div>
-      
     </div>
   );
 }
